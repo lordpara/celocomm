c64aaaa08afdd98703f0bc7e71379d80be857497
lordpara <96684703+lordpara@users.noreply.github.com>
2022-01-07 21:12:31 +0100

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@
 const ERC20_DECIMALS = 18;
 const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
 // const CeloCommContractAddressAbi = "0xc7109a3db117b3EE8b180e4f92Eae2e1Dc8e16E6";
-const CeloCommContractAddressAbi = "0xcdE4946C1001c265C5CaE4E30F3aCC7f553F1584"
+const CeloCommContractAddressAbi = "0xC73aafEBc30cF14fa4adBDff15AA75398AC12D2f"
 
 
 export {ERC20_DECIMALS, CeloCommContractAddressAbi, cUSDContractAddress}
